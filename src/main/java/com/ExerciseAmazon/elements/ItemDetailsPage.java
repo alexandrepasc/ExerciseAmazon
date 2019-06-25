ef74feee06058a04edcdb1b976aff8b9f88bb8e4
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-25 22:17:29 +0100

    Add elements, and more steps to the test.

---
+++ src/main/java/com/ExerciseAmazon/elements/ItemDetailsPage.java
@@ -0,0 +1,12 @@
+package com.ExerciseAmazon.elements;
+
+import org.openqa.selenium.By;
+
+public class ItemDetailsPage {
+
+  public static By popupKindle = By.id("a-popover-2");
+
+  public static By butPopupKindleClose = By.cssSelector("#a-popover-2 > div > header > button");
+
+  public static By labelTitle = By.id("productTitle");
+}
--- src/main/java/com/ExerciseAmazon/tests/SearchTest.java
+++ src/main/java/com/ExerciseAmazon/tests/SearchTest.java
@@ -4,6 +4,11 @@ import com.ExerciseAmazon.common.Elements;
 import com.ExerciseAmazon.common.PreTest;
 import com.ExerciseAmazon.common.Utils;
 import com.ExerciseAmazon.elements.HomePage;
+import com.ExerciseAmazon.elements.ItemDetailsPage;
+import com.ExerciseAmazon.elements.SearchResultPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -35,6 +40,28 @@ public class SearchTest extends PreTest {
     element.sendKeys(HomePage.inputSearch, "chasing Excellence");
 
     element.click(HomePage.butSearch);
+
+    Assert.assertTrue(element.getText(SearchResultPage.labelResultNumber).contains("results"));
+
+    //TODO Check this later
+    WebElement[] elements = element.getElements(SearchResultPage.listResults);
+    for (int i = 0; i < elements.length; i++) {
+
+      if (elements[i].getText().contains("Bergeron")) {
+        System.out.println(i);
+        elements[i].findElement(By.className("a-link-normal")).click();
+        //element.click(SearchResultPage.listResults, i);
+
+        break;
+      }
+    }
+
+    Assert.assertTrue(element.checkElement(ItemDetailsPage.labelTitle));
+
+    if (element.checkElement(ItemDetailsPage.popupKindle)) {
+
+      element.click(ItemDetailsPage.butPopupKindleClose);
+    }
   }
 
   @AfterMethod(alwaysRun = true)
