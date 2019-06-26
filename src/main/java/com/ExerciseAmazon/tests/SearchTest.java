b9256b8f833c00a3c3c5d6247e2a62c61ec3a51f
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-26 12:21:46 +0100

    New test.

--- src/main/java/com/ExerciseAmazon/tests/SearchTest.java
+++ src/main/java/com/ExerciseAmazon/tests/SearchTest.java
@@ -5,6 +5,7 @@ import com.ExerciseAmazon.common.PreTest;
 import com.ExerciseAmazon.common.Utils;
 import com.ExerciseAmazon.elements.HomePage;
 import com.ExerciseAmazon.elements.ItemDetailsPage;
+import com.ExerciseAmazon.elements.ReviewsPage;
 import com.ExerciseAmazon.elements.SearchResultPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -48,9 +49,8 @@ public class SearchTest extends PreTest {
     for (int i = 0; i < elements.length; i++) {
 
       if (elements[i].getText().contains("Bergeron")) {
-        System.out.println(i);
+
         elements[i].findElement(By.className("a-link-normal")).click();
-        //element.click(SearchResultPage.listResults, i);
 
         break;
       }
@@ -58,10 +58,56 @@ public class SearchTest extends PreTest {
 
     Assert.assertTrue(element.checkElement(ItemDetailsPage.labelTitle));
 
+    Utils.scrollToElement(driver, element.getElement(ItemDetailsPage.butAllReviews));
+
     if (element.checkElement(ItemDetailsPage.popupKindle)) {
 
       element.click(ItemDetailsPage.butPopupKindleClose);
     }
+  }
+
+  @Test(enabled = true, invocationCount = 1)
+  public void searchReviewsTest()
+    throws Exception {
+
+    driver.get(
+        "https://www.amazon.co.uk/Chasing-Excellence-Building-Fittest-Athletes/product-reviews/1619617277/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1");
+
+    element = new Elements(driver);
+
+    while (!searchReviews()) {
+
+      if (element.getElement(ReviewsPage.butNext).isEnabled()) {
+
+        element.click(ReviewsPage.butNext);
+      }
+    }
+  }
+
+  private boolean searchReviews()
+    throws Exception {
+
+    driver.navigate().refresh();
+
+    element = new Elements(driver);
+
+    WebElement[] elements;
+
+    elements = element.getElements(driver, ReviewsPage.listResultsItems);
+
+    String aux;
+    for (WebElement elem : elements) {
+      //System.out.println(elem.getText());
+      aux = elem.findElement(ReviewsPage.listResultsItemsUser).getText();
+      if (aux.contains("Cerith Leighton Watkins")) {
+        System.out.println("found");
+        Assert.assertTrue(true);
+        return true;
+      }
+    }
+    //System.out.println("page");
+
+    return false;
   }
 
   @AfterMethod(alwaysRun = true)
