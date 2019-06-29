0f4a365a675c21d815e3b332ebbd91fb7f56f90c
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-29 18:19:26 +0100

    New dependency added. New elements. Add steps on test.

--- src/main/java/com/ExerciseAmazon/elements/JobSearchHomePage.java
+++ src/main/java/com/ExerciseAmazon/elements/JobSearchHomePage.java
@@ -6,12 +6,17 @@ public class JobSearchHomePage {
 
   public static By inputSearch = By.id("search_typeahead");
 
-  public static By inputLocation = By.id("location-typeahead");
+  public static By inputLocation = By.cssSelector("#home-search > div > form > div.row.nopadding.location-search-bar "
+      + "> div.col-12.col-md-4 > div > span > input");
 
   public static By selectLocation = By.cssSelector("#home-search > div > form > div.row.nopadding.location-search-bar "
       + "> div.col-12.col-md-4 > div > span > div > div");
 
-  public static By selectLocationList = By.cssSelector("div");
+  public static By selectLocationList = By.cssSelector("div > div");
+
+  public static By selectLocationTitle1 = By.cssSelector("div:nth-child(2)");
+
+  public static By selectLocationTitle2 = By.cssSelector("div:nth-child(3)");
 
   public static By butSearch = By.id("search-button");
 }
--- src/main/java/com/ExerciseAmazon/tests/SearchJobTest.java
+++ src/main/java/com/ExerciseAmazon/tests/SearchJobTest.java
@@ -3,9 +3,14 @@ package com.ExerciseAmazon.tests;
 import com.ExerciseAmazon.common.Elements;
 import com.ExerciseAmazon.common.PreTest;
 import com.ExerciseAmazon.common.Utils;
+import com.ExerciseAmazon.elements.JobSearchHomePage;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class SearchJobTest extends PreTest {
 
@@ -14,16 +19,48 @@ public class SearchJobTest extends PreTest {
   @Parameters({ "browser", "gui" })
   @BeforeMethod(alwaysRun = true)
   public void beforeMethod(String browser, String gui)
-      throws Exception {
+    throws Exception {
 
     beforeTest(browser, gui);
 
     driver.get(Utils.getJobSearchUrl());
   }
 
+  @Test(enabled = true, invocationCount = 1)
+  public void searchPortugalSetubalTest()
+    throws Exception {
+
+    element = new Elements(driver);
+
+    String search = "Portugal, setubal";
+    //Assert.assertTrue(element.checkElement(JobSearchHomePage.inputLocation));
+    element.sendKeys(JobSearchHomePage.inputLocation, search);
+
+    //return Normalizer.normalize(string, Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+    WebElement[] results = element.getElement(JobSearchHomePage.selectLocation)
+        .findElements(JobSearchHomePage.selectLocationList).toArray(new WebElement[0]);
+    System.out.println(results.length);
+    for (WebElement result : results) {
+
+      if (StringUtils.stripAccents(search).toLowerCase().contains(StringUtils
+          .stripAccents(result.findElement(JobSearchHomePage.selectLocationTitle1).getText().toLowerCase()))) {
+
+        if (StringUtils.stripAccents(search).toLowerCase().contains(StringUtils
+            .stripAccents(result.findElement(JobSearchHomePage.selectLocationTitle2).getText().toLowerCase()))) {
+
+          result.click();
+          break;
+        }
+      }
+      //System.out.println("result: " + result.getText());
+
+      //System.out.println("title2: " + result.findElement(JobSearchHomePage.selectLocationTitle2).getText());
+    }
+  }
+
   @AfterMethod(alwaysRun = true)
   public void afterMethod()
-      throws Exception {
+    throws Exception {
 
     afterTest();
   }
