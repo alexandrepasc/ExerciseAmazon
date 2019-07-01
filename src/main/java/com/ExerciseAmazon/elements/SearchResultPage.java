47c1f56452725ead680e619b0a64fbcfc139a2db
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-01 18:59:55 +0100

    Correct test, add new element.

--- src/main/java/com/ExerciseAmazon/elements/SearchResultPage.java
+++ src/main/java/com/ExerciseAmazon/elements/SearchResultPage.java
@@ -10,6 +10,8 @@ public class SearchResultPage {
   public static By listResults = By.cssSelector(
       "#search > div.sg-row > div.sg-col-20-of-24.sg-col-28-of-32.sg-col-16-of-20.sg-col.s-right-column.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(4) > div.s-result-list.s-search-results.sg-row > div");
 
+  public static By listResultsImage = By.cssSelector("div > div > div >div:nth-child(2) > div > div > div");
+
   public static By listResultsItemWriter = By.cssSelector(
       "div:nth-child(1) > div > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-16-of-24.sg-col-12-of-20.sg-col-24-of-32.sg-col.sg-col-28-of-36.sg-col-20-of-28 > div > div:nth-child(1) > div > div > div:nth-child(1) > div > a");
 
